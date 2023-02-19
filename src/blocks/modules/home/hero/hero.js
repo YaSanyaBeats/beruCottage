0ded1243d2983929b749633d45afd6cadab23770
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2023-02-19 23:20:23 +0700

    Add parallax on hero

--- src/blocks/modules/home/hero/hero.js
+++ src/blocks/modules/home/hero/hero.js
@@ -2,9 +2,18 @@
 // core version + navigation, pagination modules:
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
-console.log(123);
+
+import simpleParallax from 'simple-parallax-js';
 
 const headerBackgroundSlider = new Swiper('.header__swiper', {
     // Optional parameters
     direction: 'horizontal',
 });
+
+let heroVideos = document.querySelectorAll('.hero__video');
+if(heroVideos.length > 0) {
+    new simpleParallax(heroVideos, {
+        scale: 0.1,
+        overflow: true,
+    });
+}

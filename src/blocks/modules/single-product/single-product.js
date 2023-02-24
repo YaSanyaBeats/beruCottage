4f3f529f5ed10ae4aa7ce276e3e7fbb30afea291
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2023-02-24 23:18:21 +0700

    Thumb slider on Single Product

--- src/blocks/modules/single-product/single-product.js
+++ src/blocks/modules/single-product/single-product.js
@@ -1,16 +1,16 @@
-import Swiper, {Pagination} from 'swiper';
-Swiper.use([Pagination]);
+import Swiper, {Pagination, Thumbs} from 'swiper';
+Swiper.use([Pagination, Thumbs]);
 
-const blogSlider = new Swiper('.product__swiper', {
+const layoutsSlider = new Swiper('.product__layouts-swiper', {
     // Optional parameters
-    slidesPerView: '1',
-    slidesPerGroup: '1',
+    slidesPerView: 1,
+    slidesPerGroup: 1,
     spaceBetween: 50,
     grabCursor: true,
     
     // If we need pagination
     pagination: {
-        el: '.product__swiper .product__slider-dots',
+        el: '.product__layouts-swiper .product__layouts-slider-dots',
     },
 
     breakpoints: {
@@ -21,3 +21,19 @@ const blogSlider = new Swiper('.product__swiper', {
     }
 
 });
+
+const mainThumbProductSlider = new Swiper(".product__info-slides", {
+    spaceBetween: 30,
+    slidesPerView: 3,
+    watchSlidesProgress: true,
+});
+
+const mainProductSlider = new Swiper('.product__main-slider', {
+    // Optional parameters
+    spaceBetween: 50,
+    grabCursor: true,
+    thumbs: {
+        swiper: mainThumbProductSlider,
+      },
+});
+
